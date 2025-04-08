3e2b0c99b2271554f09d65103ee66e4eba5df060
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-08 15:45:10 +0200

    es03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const firstChar = (word, initial) => {
+  const filteredName = [];
+  for (i = 0; i < word.length; i++) {
+    const currentChar = word[i];
 
+    if (currentChar[0] === initial) {
+      filteredName.push(currentChar);
+    }
+  }
+  return filteredName;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(firstChar(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
